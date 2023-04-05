89064d58891f3856bf5c81a61cfe488dde0c86f9
stoclet sylvain <cronosu@hotmail.fr>
2023-04-05 08:58:36 +0200

    a

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const selectedIndexes = [];
+const descrition = document.querySelectorAll(".description");
+const img = document.querySelectorAll(".carte");
+const glow = document.querySelectorAll(".glow");
+
+
 let data;
 
-fetch('carte.json')
+const fichierjson = fetch('carte.json')
   .then(response => response.json())
   .then(jsonData => {
     data = jsonData;
+
     for (let i = 0; i < 12; i++) {
       let randomNum;
-
       do {
         randomNum = Math.floor(Math.random() * data.length);
       } while (selectedIndexes.includes(randomNum));
@@ -16,30 +21,47 @@ fetch('carte.json')
       let jsonData = data[randomNum];
       let name = jsonData.name;
       let carteImg = document.querySelector(`.carte-img${i}`);
+      let ability = data[randomNum].ability;
+      let descriptions = document.querySelectorAll(`.description${i}`);
+      descriptions.forEach(description => {
+        description.innerText = ability;
+      });
+
       carteImg.src = `image/${name}.webp?v=20`;
-    }
+    };
   })
   .catch(error => console.error(error));
 
-function changeRandomImage(imgElement) {
-  let randomNum;
 
-  do {
-    randomNum = Math.floor(Math.random() * data.length);
-  } while (selectedIndexes.includes(randomNum));
 
-  selectedIndexes.push(randomNum);
-  let jsonData = data[randomNum];
-  let name = jsonData.name;
-  imgElement.src = `image/${name}.webp?v=20`;
-}
 
-const btns = document.querySelectorAll('.btn');
-const cardImgs = document.querySelectorAll('.card img');
-
-for (let i = 0; i < cardImgs.length; i++) {
-  btns[i].addEventListener('click', () => changeRandomImage(cardImgs[i]));
-}
+  function changeRandomImage(imgElement, descriptions) {
+    let randomNum;
+  
+    do {
+      randomNum = Math.floor(Math.random() * data.length);
+    } while (selectedIndexes.includes(randomNum));
+  
+    selectedIndexes.push(randomNum);
+    let jsonData = data[randomNum];
+    let name = jsonData.name;
+    imgElement.src = `image/${name}.webp?v=20`;
+  
+    let ability = data[randomNum].ability;
+  
+    descriptions.forEach(description => {
+      description.innerText = ability;
+    });
+  }
+  
+  const btns = document.querySelectorAll('.btn');
+  const cardImgs = document.querySelectorAll('.card img');
+  
+  for (let i = 0; i < cardImgs.length; i++) {
+    const descriptions = document.querySelectorAll(`.description${i}`);
+    btns[i].addEventListener('click', () => changeRandomImage(cardImgs[i], descriptions));
+  }
+  
 
 
 
@@ -65,18 +87,37 @@ card.forEach(el => {
     el.querySelector('.glow').style.transform = `rotateX(${angleX}deg) rotateY(${angleY}deg)`
 
     el.querySelector('.glow').style.background = `radial-gradient(circle at ${glowX}% ${glowY}%,rgb(217, 235, 252, 0.5), transparent)`
-
-
-
-
   })
+
   el.addEventListener("mouseleave", () => {
+
+
+
     el.querySelector('.glow').style.background.remove;
-    el.querySelector('.glow').style.transform = "rotateX(0) rotateY(0)"
-    el.querySelector('img').style.transform = "rotateX(0) rotateY(0)"
+    el.querySelector('.glow').style.background.remove;
+    el.querySelector('.glow').style.transform = "rotateX(0) rotateY(0)";
+    el.querySelector('img').style.transform = "rotateX(0) rotateY(0)";
     el.querySelector('.glow').style.background = '';
   })
 })
 
+for (let i = 0; i < glow.length; i++) {
+  glow[i].addEventListener("click", function () {
+    glow[i].style.transition = "transform 0.5s ease";
+    img[i].style.transition = "transform 0.5s ease";
+
+    img[i].style.filter = 'brightness(0%)';
+
+    descrition[i].classList.add("descriptionVisible");
+  });
+}
+
+
+for (let i = 0; i < glow.length; i++) {
+  glow[i].addEventListener("mouseleave", function () {
+    descrition[i].classList.remove("descriptionVisible");
+    img[i].style.filter = 'none';
+  });
+}
 
 
